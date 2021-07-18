0bb5f8f15d7a534688bfcd43018cc44e9a3fd949
huangk27 <85188995+huangk27@users.noreply.github.com>
2021-07-17 22:10:25 -0400

    Add files via upload

---
+++ instructor.py
@@ -0,0 +1,11 @@
+from user import user
+
+class instructor(user):
+    def __init__(self, firstName, lastName, IDnumber):
+        super().__init__(firstName, lastName, IDnumber)
+        
+    def printSchedule(self):
+        print("Print schedule")
+
+    def printRoster(self):
+        print("Print all rosters and students in each roster")
