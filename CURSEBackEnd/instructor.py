798d59184efd27624f7f37bf13d1d332a826aa03
huangk27 <85188995+huangk27@users.noreply.github.com>
2021-07-22 19:53:00 -0400

    Added comments

--- CURSEBackEnd/instructor.py
+++ CURSEBackEnd/instructor.py
@@ -1,4 +1,4 @@
-#Instr
+#Instructor class done by Kevin Huang(50%) and Hank Pham(50%)
 
 from user import user
 
@@ -7,9 +7,11 @@ class instructor(user):
         super().__init__(firstName, lastName, IDnumber)
         
     def getSchedule(self):
+        """Get schedule for instructor"""
         print("Print schedule")
 
     def getRosterFromACourse(self, CRN):
+        """Get Roster from a course"""
         self.roster = self.sql.select_from_where_in("STUDENT", "*", "ID", "ROSTER", "StudentID", "CRN", CRN)
         return self.roster
         
--- CURSEBackEnd/student.py
+++ CURSEBackEnd/student.py
@@ -4,10 +4,12 @@ from user import user
 
 class student(user):
     def __init__(self, firstName, lastName, IDnumber):
+        """initialize parent class"""
         super().__init__(firstName, lastName, IDnumber)
         
     def registerCourseByCRN(self, CRN):
-        """adds student ID to the ROSTER """
+        """adds student ID and CRN to the ROSTER table """
+
         if len(self.searchCourseByCRN(CRN)) !=0:
             self.value = f"{self.ID}, {CRN}"
             self.sql.insert_into_table_values("ROSTER", self.value)
@@ -15,12 +17,15 @@ class student(user):
         else:
             return False
         
-
     def dropCourseByCRN(self, CRN):
+        """Deletes student ID and CRN from ROSTER table"""
+
         self.value = f"{CRN} AND StudentID = {self.ID}"
         self.sql.delete_from_table_where("ROSTER", "CRN", self.value)
 
     def getSchedule(self):
+        """Prints out the courses from the COURSE table that have the same CRN as the one from ROSTER table and looks at thr CRN of the student ID entered"""
+        
         self.courses = self.sql.select_from_where_in("COURSE", "*", "CRN", "ROSTER", "CRN", "StudentID", self.ID)
         return self.courses
         
