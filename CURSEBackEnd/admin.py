54dcc8dbe5c417aac8bdcde61c47bc26d13bf6b7
HkPPP <phamh4@wit.edu>
2021-08-06 18:16:32 -0400

    Updated

--- CURSEBackEnd/admin.py
+++ CURSEBackEnd/admin.py
@@ -1,4 +1,4 @@
-#Admin Class was done by Hank Pham
+# Admin Class was done by Hank Pham
 from typing import List
 
 try:
@@ -8,17 +8,18 @@ except ModuleNotFoundError:
     sys.path.append(".")
     from CURSEBackEnd.user import user
 
+
 class admin(user):
     def __init__(self, firstName, lastName, IDnumber):
         super().__init__(firstName, lastName, IDnumber)
-        
-    def getAllStudent(self)->List:
+
+    def getAllStudent(self) -> List:
         return self.sql.select_from("STUDENT", "*")
-    
-    def getAllInstructor(self)->List:
+
+    def getAllInstructor(self) -> List:
         return self.sql.select_from("INSTRUCTOR", "*")
 
-    def addNewCourse(self, courseName,  department = "TBA", instructor = "TBA", time = 0, day = "TBA", semester = "TBA", year = 0, credits = 0)->None:
+    def addNewCourse(self, courseName,  department="TBA", instructor="TBA", time=0, day="TBA", semester="TBA", year=0, credits=0) -> None:
         """Add new course to database"""
 
         lastID = 30001 + int(self.sql.get_last_rowID("COURSE"))
@@ -26,17 +27,17 @@ class admin(user):
         value = f"'{CRN}', '{courseName}', '{department}', '{instructor}', {time}, '{semester}', '{day}', {year}, {credits}"
         self.sql.insert_into_table_values("COURSE", value)
 
-    def removeCourseByCRN(self,CRN)->None:
+    def removeCourseByCRN(self, CRN) -> None:
         """Remove course by CRN in COURSE table"""
         CRN = f"'{CRN}'"
-        self.sql.delete_from_table_where("COURSE", "CRN", CRN) 
+        self.sql.delete_from_table_where("COURSE", "CRN", CRN)
 
-    def removeCourseByName(self,name)->None:
+    def removeCourseByName(self, name) -> None:
         """Remove course by name in COURSE table"""
         name = f"'{name}'"
-        self.sql.delete_from_table_where("COURSE", "TITLE", name) 
+        self.sql.delete_from_table_where("COURSE", "TITLE", name)
 
-    def addNewStudent(self, firstName, lastName, graduation, major = "TBA")->None:
+    def addNewStudent(self, firstName, lastName, graduation, major="TBA") -> None:
         """Add new student to STUDENT table"""
 
         student_count = int(self.sql.get_last_rowID("STUDENT"))
@@ -44,8 +45,8 @@ class admin(user):
         email = self.createNewEmail(firstName, lastName, "STUDENT")
         value = f"{studentID}, '{firstName}', '{lastName}', {graduation}, '{major}', '{email}'"
         self.sql.insert_into_table_values("STUDENT", value)
-        
-    def addNewInstructor(self, first, last, hire_year, title="TBA", dept="TBA")->None:
+
+    def addNewInstructor(self, first, last, hire_year, title="TBA", dept="TBA") -> None:
         """Add new instructor to INSTRUCTOR table"""
 
         ins_count = int(self.sql.get_last_rowID("INSTRUCTOR"))
@@ -54,13 +55,13 @@ class admin(user):
         value = f"{insID}, '{first}', '{last}', '{title}', {hire_year}, '{dept}', '{email}'"
         self.sql.insert_into_table_values("INSTRUCTOR", value)
 
-    def removeStudentByID(self, id)->None:
+    def removeStudentByID(self, id) -> None:
         """"Remove a student from STUDENT, ROSTER tables by matching ID """
 
-        self.sql.delete_from_table_where("STUDENT", "ID", id) 
+        self.sql.delete_from_table_where("STUDENT", "ID", id)
         self.sql.delete_from_table_where("ROSTER", "StudentID", id)
-    
-    def removeProfessorByID(self, id)->None:
+
+    def removeProfessorByID(self, id) -> None:
         """Remove a professor from INSTRUCTOR and COURSE tables"""
 
         self.sql.delete_from_table_where("INSTRUCTOR", "ID", id)
@@ -68,24 +69,19 @@ class admin(user):
         condt = f"INSTRUCTOR = {id}"
         self.sql.update_set_where("COURSE", new_info, condt)
 
-
     def createNewEmail(self, firstName, lastName, table) -> str:
         """Create a new email in the format 'lastf0'"""
 
         last_and_first_init = f"{lastName.lower()}{firstName[0:1].lower()}"
         condt = f"'{last_and_first_init}%'"
-        email_index = len(self.sql.get_partial_matches(table, "EMAIL","EMAIL",condt))
-        email =f"{last_and_first_init}{email_index}"
+        email_index = len(self.sql.get_partial_matches(
+            table, "EMAIL", "EMAIL", condt))
+        email = f"{last_and_first_init}{email_index}"
         return email
 
- 
-
-
     # TODO below
-    
 
-    
-    def addStudentToCourse(self, id, CRNcode): 
+    def addStudentToCourse(self, id, CRNcode):
         """Add Student ID along with Course ID to ROSTER table"""
 
         print("Student " + id + " added to course " + CRNcode)
@@ -96,11 +92,11 @@ class admin(user):
         print("Student " + id + " removed from course " + CRNcode)
 
 
-
 def main():
     # ad = admin("Barack","Obama","30001")
     # ad.addNewStudent("Issac","Newton",1668,"BSAS")
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
